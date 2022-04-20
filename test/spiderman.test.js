f3d33d482ebb88d1d74b9589f4d128a990f019a9
Julieta Delgado <julidmeza@gmail.com>
2022-04-20 00:08:13 -0500

    Step two of TDD: Write the functional code until the test passes

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,3 +1,5 @@
+const Spiderman = require('./../app/spiderman.js')
+
 describe('Unit Test for Spiderman class', () =>{
     test('1. Create an spiderman object', () => {
         //I want to make an instance of an Spiderman object with this information
@@ -5,7 +7,7 @@ describe('Unit Test for Spiderman class', () =>{
         expect(andrewGarfiel.name).toBe("Spiderman Sony")
         expect(andrewGarfiel.age).toBe(31)
         expect(andrewGarfiel.actor).toBe("Andrew Garfield")
-        expect(andrewGarfiel.movies).toBe(31)
+        expect(andrewGarfiel.movies).toBe(2)
         expect(andrewGarfiel.studio).toBe("Sony")
     })
 })
