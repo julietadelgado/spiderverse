5e85df5b8d08f90b6daf5cb9d9c3bc667bb5254a
Julieta Delgado <julidmeza@gmail.com>
2022-04-20 00:13:53 -0500

    Adding a new failed test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,6 @@
 const Spiderman = require('./../app/spiderman.js')
 
-describe('Unit Test for Spiderman class', () =>{
+describe('Unit Test for Spiderman class', () => {
     test('1. Create an spiderman object', () => {
         //I want to make an instance of an Spiderman object with this information
         const andrewGarfiel = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
@@ -9,5 +9,9 @@ describe('Unit Test for Spiderman class', () =>{
         expect(andrewGarfiel.actor).toBe("Andrew Garfield")
         expect(andrewGarfiel.movies).toBe(2)
         expect(andrewGarfiel.studio).toBe("Sony")
+    })
+    test('2. Use the method getInfo()', () => {
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
     })
 })
