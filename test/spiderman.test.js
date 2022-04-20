996bc1c03d08b7506ac4ba082b13c8c47a73101a
Julieta Delgado <julidmeza@gmail.com>
2022-04-20 00:00:55 -0500

    Step one of TDD: Write a test for the next bit of functionality you want to add

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,11 @@
-describe('Starting with TDD', () =>{
-    test('1. Failed Test', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(5)
+describe('Unit Test for Spiderman class', () =>{
+    test('1. Create an spiderman object', () => {
+        //I want to make an instance of an Spiderman object with this information
+        const andrewGarfiel = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+        expect(andrewGarfiel.name).toBe("Spiderman Sony")
+        expect(andrewGarfiel.age).toBe(31)
+        expect(andrewGarfiel.actor).toBe("Andrew Garfield")
+        expect(andrewGarfiel.movies).toBe(31)
+        expect(andrewGarfiel.studio).toBe("Sony")
     })
 })
